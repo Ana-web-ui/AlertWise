406178ce8f73ef22568540727a844cb346b3dc76
ana leite <anacarolinaleite1401@gmail.com>
2025-06-05 08:58:47 -0300

    frontend

--- src/Screens/User.jsx
+++ src/Screens/User.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Avatar from "../components/Avatar/Avatar";
 
 const User = () => {
   const [isLogin, setIsLogin] = useState(true); // Estado para controlar qual formulário mostrar
@@ -8,7 +7,7 @@ const User = () => {
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <Avatar />
+
           <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">
             {isLogin ? "Sign in to your account" : "Create new account"}
           </h2>
@@ -20,7 +19,7 @@ const User = () => {
             <form action="#" method="POST" className="space-y-6">
               <div>
                 <label htmlFor="email" className="block text-sm/6 font-medium text-white">
-                  Email address
+                  Email
                 </label>
                 <div className="mt-2">
                   <input
@@ -37,11 +36,11 @@ const User = () => {
               <div>
                 <div className="flex items-center justify-between">
                   <label htmlFor="password" className="block text-sm/6 font-medium text-white">
-                    Password
+                    Senha
                   </label>
                   <div className="text-sm">
                     <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                      Forgot password?
+                      Esqueceu a senha?
                     </a>
                   </div>
                 </div>
@@ -60,9 +59,9 @@ const User = () => {
               <div className="p-2">
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
                 >
-                  Sign in
+                  Entrar
                 </button>
               </div>
             </form>
@@ -71,7 +70,7 @@ const User = () => {
             <form action="#" method="POST" className="space-y-6">
               <div>
                 <label htmlFor="name" className="block text-sm/6 font-medium text-white">
-                  Full Name
+                  Nome completo
                 </label>
                 <div className="mt-2">
                   <input
@@ -86,7 +85,7 @@ const User = () => {
 
               <div>
                 <label htmlFor="email" className="block text-sm/6 font-medium text-white">
-                  Email address
+                  Email
                 </label>
                 <div className="mt-2">
                   <input
@@ -102,7 +101,7 @@ const User = () => {
 
               <div>
                 <label htmlFor="password" className="block text-sm/6 font-medium text-white">
-                  Password
+                  Senha
                 </label>
                 <div className="mt-2">
                   <input
@@ -114,28 +113,14 @@ const User = () => {
                   />
                 </div>
               </div>
-
-              <div>
-                <label htmlFor="confirmPassword" className="block text-sm/6 font-medium text-white">
-                  Confirm Password
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="confirmPassword"
-                    name="confirmPassword"
-                    type="password"
-                    required
-                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                  />
-                </div>
-              </div>
+               
 
               <div className="p-2">
                 <button
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Create Account
+                  Criar a conta
                 </button>
               </div>
             </form>
@@ -147,8 +132,8 @@ const User = () => {
               className="text-sm font-semibold text-indigo-600 hover:text-indigo-500"
             >
               {isLogin 
-                ? "Don't have an account? Create one" 
-                : "Already have an account? Sign in"}
+                ? "Não tem conta? Crie uma agora" 
+                : "Já tem conta? Entre"}
             </button>
           </div>
         </div>
